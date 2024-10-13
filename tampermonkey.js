13bac7b710a84f0103610498cf568e7880d123d1
marcos10pc <30839781+marcos10pc@users.noreply.github.com>
2024-10-12 23:55:31 -0300

    Atualizar o tampermonkey.js

--- tampermonkey.js
+++ tampermonkey.js
@@ -47,7 +47,7 @@ function abacate(originalUrl) {
                     const porra_answer = await pre_answer.json();
                     const caralhos = porra_answer.pageProps.content.children[0].list;
                     const damn = caralhos.find(resposta => resposta.isCorrect === true);
-                    console.log(`[DEBUG] -- ${damn} --`)
+                    console.log(`[DEBUG] -- ${JSON.stringify(damn)} --`)
                     const buttons = document.querySelectorAll('.exercise-answer__button');
 
                     buttons.forEach(button => {
